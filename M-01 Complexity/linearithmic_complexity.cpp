1f0c4a264eaf1ac2b1a460b295b5b48aef3c3c8f
am-sazid <amsazid.cse@gmail.com>
2025-12-19 16:21:36 +0600

    Module 1 Complete

---
+++ M-01 Complexity/linearithmic_complexity.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin>> n ;
+
+    for (int i = 0; i < n; i++) // O(N)
+    {
+        for (int j = 1; j <= n; j*=2 ) // O(logN)
+        {
+            cout<<"Hello"<<endl;
+        }
+        
+    }
+    
+    return 0;
+}
