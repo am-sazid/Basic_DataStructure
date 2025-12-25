9bb071da1707188601cbb95625e963f5e4187e4b
am-sazid <amsazid.cse@gmail.com>
2025-12-25 23:42:50 +0600

    referance of pointer

---
+++ M-05 Singly Linked List operation/refermce_of_pointer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int * p)
+{
+    // int y = 100;
+    p = NULL;
+    cout << p << endl;
+}
+
+int main()
+{
+    int x = 10;
+
+    int *p = &x;
+    // cout << *p << endl;
+
+    fun(p);
+    cout << p << endl;
+
+    return 0;
+}
