6bf69c48816cae65d2a0c57ec95cdeeadb63bb1d
am-sazid <amsazid.cse@gmail.com>
2026-01-17 00:26:05 +0600

    Module-12 added

---
+++ M-12 Queue Implementation/Queue_implement_use_stack.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+public:
+
+    queue<int> st;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        st.push(x);
+    }
+    
+    int pop() {
+        queue<int> st2;
+        
+    }
+    
+    int peek() {
+        
+    }
+    
+    bool empty() {
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
