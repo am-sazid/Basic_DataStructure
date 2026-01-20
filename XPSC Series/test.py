065c80adf482ae20dbf3f86d4844447bb86902e1
am-sazid <amsazid.cse@gmail.com>
2026-01-21 03:01:00 +0600

    XPSC 02

---
+++ XPSC Series/test.py
@@ -0,0 +1,17 @@
+import math 
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+D = b*b - 4*a*c
+
+if D > 0:
+    x1 = (-b + math.sqrt(D)) / (2*a)
+    x2 = (-b - math.sqrt(D)) / (2*a)
+    print("Roots:", x1, x2)
+elif D == 0:
+    x = -b / (2*a)
+    print("Root:", x)
+else:
+    print("No real root")
