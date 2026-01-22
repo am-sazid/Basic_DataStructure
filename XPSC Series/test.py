c631e983ebdd29f76c9818e580407de978ed82bd
am-sazid <amsazid.cse@gmail.com>
2026-01-22 23:00:19 +0600

    Python Test

--- XPSC Series/test.py
+++ XPSC Series/test.py
@@ -1,17 +1,17 @@
 import math 
 
-a = float(input("Enter a: "))
-b = float(input("Enter b: "))
-c = float(input("Enter c: "))
+a = float(input("Enter a "))
+b = float(input("Enter b "))
+c = float(input("Enter c "))
 
 D = b*b - 4*a*c
 
 if D > 0:
     x1 = (-b + math.sqrt(D)) / (2*a)
     x2 = (-b - math.sqrt(D)) / (2*a)
-    print("Roots:", x1, x2)
+    print("roots:", x1, x2)
 elif D == 0:
     x = -b / (2*a)
-    print("Root:", x)
+    print("root:", x)
 else:
     print("No real root")
